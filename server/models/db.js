4a91cc71e8e29f2ae8048d02a7e84963112b9f7a
Madeleine Chance <mechance782@gmail.com>
2025-10-02 11:45:34 -0700

    set up db connection with sequelize

---
+++ server/models/db.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+
+dotenv.config({
+    path: './db.env'
+});
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, DB_PORT } = process.env;
+
+const sequelize = new Sequelize(
+    DB_DATABASE, DB_USER, DB_PASSWORD, {
+        host: DB_HOST,
+        port: DB_PORT,
+        dialect: 'mysql',
+        logging: query => console.log(`SQL: ${query}` )
+    }
+);
+
+try {
+    await sequelize.authenticate();
+    console.log('Connected successfully');
+} catch (error) {
+    console.error('Unable to connect:', error);
+}
+
+export default sequelize;
