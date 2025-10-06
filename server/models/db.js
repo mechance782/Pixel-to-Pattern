2dcaaaf053190430a395c0e2f8f7975a04bc4644
Kimberly Mageary <kimberlymageary@gmail.com>
2025-10-06 16:55:55 -0700

    Wrapping up get by ID

--- server/models/db.js
+++ server/models/db.js
@@ -10,9 +10,9 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, DB_PORT } = process.env;
 
 // set up sequelize connection to database
 const sequelize = new Sequelize(
-    DB_DATABASE, DB_USER, DB_PASSWORD, {
-        host: DB_HOST,
-        port: DB_PORT,
+    'pixel_pattern_db', 'remote', 'Pixel372', {
+        host: '143.198.75.243',
+        port: 3306,
         dialect: 'mysql',
         logging: query => console.log(`SQL: ${query}` )
     }
