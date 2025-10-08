b0b1589157938a7eb53150f80ae340ef0e01162f
Madeleine Chance <mechance782@gmail.com>
2025-10-08 15:48:03 -0700

    start on main page

--- pixel2pattern/src/components/MainHome.jsx
+++ pixel2pattern/src/components/MainHome.jsx
@@ -1,3 +1,32 @@
+"use client";
+import { useEffect, useState } from "react";
 export default function MainHome() {
+
+    const [ pixelPosts, setPixelPosts] = useState([]);
     
+    useEffect(() => {
+
+        const fetchPosts = async () => {
+            try {
+                const res = await fetch("http://localhost:3001/patterns");
+                if (!res.ok) throw new Error("failed to fetch posts");
+                const posts = await res.json();
+                setPixelPosts(posts);
+            } catch (error) {
+                console.error('failed to fetch posts ', error);
+            } 
+        }
+
+        fetchPosts();
+        
+    })
+
+    return(
+        <>
+        {pixelPosts.map((post, index) => (
+            <h1>{console.log(post)}</h1>
+        ))}
+        
+        </>
+    )
 }
