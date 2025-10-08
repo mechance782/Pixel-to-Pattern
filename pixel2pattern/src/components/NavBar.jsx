21e3875e4408e91b185cac4bf0e5a1845e955b78
Kimberly Mageary <kimberlymageary@gmail.com>
2025-10-08 16:22:14 -0700

    Basic nav bar setup

--- pixel2pattern/src/components/NavBar.jsx
+++ pixel2pattern/src/components/NavBar.jsx
@@ -1,3 +1,25 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { Button } from '@mui/material';
+
 export default function NavBar() {
+
+    return(
+        <>
+            <AppBar position="static" sx={{pr: '10%'}}>
+                <Toolbar>
+                    <img alt="Logo" style={{width: '15%'}} src="logo.png" />
+                    <Typography sx={{ flexGrow: 1 }} variant="h6">Pixel2Pattern</Typography>
+
+                    <Button color="inherit">Home</Button>
+                    <Button color="inherit">Create</Button>
+                    <Button color="inherit">FAQs</Button>
+                </Toolbar>
+            </AppBar>
+        </>
+    )
     
 }
