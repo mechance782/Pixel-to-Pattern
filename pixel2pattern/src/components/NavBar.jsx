3f519d0890dbb6e8fcfcf311b9f059281bc00c53
Kimberly Mageary <kimberlymageary@gmail.com>
2025-10-08 20:08:01 -0700

    Finished basic navigation bar

--- pixel2pattern/src/components/NavBar.jsx
+++ pixel2pattern/src/components/NavBar.jsx
@@ -4,22 +4,26 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { Button } from '@mui/material';
+import Box from '@mui/material/Box';
 
 export default function NavBar() {
 
     return(
         <>
-            <AppBar position="static" sx={{pr: '10%'}}>
+            <AppBar position="static" sx={{pr: '10%', background: 'linear-gradient(90deg, #bbe1ebff 0%, #318397ff 65%)',}}>
                 <Toolbar>
-                    <img alt="Logo" style={{width: '15%'}} src="logo.png" />
-                    <Typography sx={{ flexGrow: 1 }} variant="h6">Pixel2Pattern</Typography>
+                    <Box sx={{display:'flex', alignItems:'center'}}>
+                        <img alt="Logo" style={{margin: '15px', height: '250px',width: 'auto',marginRight:'1rem',}} src="logo.png" />
+                    </Box>
+                    <Typography sx={{ flexGrow: 1, ml: '3rem'}} variant="h3">Pixel2Pattern</Typography>
 
-                    <Button color="inherit">Home</Button>
-                    <Button color="inherit">Create</Button>
-                    <Button color="inherit">FAQs</Button>
+                    <Button sx={{m: '2rem', fontSize:'1.5rem'}} color="inherit">Home</Button>
+                    <Button sx={{m: '2rem', fontSize:'1.5rem'}} color="inherit">Create</Button>
+                    <Button sx={{m: '2rem', fontSize:'1.5rem'}} color="inherit">FAQs</Button>
                 </Toolbar>
             </AppBar>
         </>
     )
     
 }
+
