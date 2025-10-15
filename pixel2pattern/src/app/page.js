a139c6cb91b9d7a600fd03cd034c3a3549c974de
Madeleine Chance <mechance782@gmail.com>
2025-10-14 17:01:06 -0700

    set up grid for home page

--- pixel2pattern/src/app/page.js
+++ pixel2pattern/src/app/page.js
@@ -1,13 +1,24 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import MainHome from "@/components/MainHome.jsx";
+import { Grid } from "@mui/material";
+import PostsCollection from "@/components/PostsCollection.jsx";
 import NavBar from "@/components/NavBar.jsx";
+import SideBar from "@/components/SideBar";
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <MainHome />
+      <Grid container spacing='1em' sx={{margin: '1em'}}>
+        <Grid size={{sm: 12, md: 8}}>
+          <PostsCollection />
+        </Grid>
+        
+        <Grid size={{sm: 12, md: 4}}>
+          <SideBar />
+        </Grid>
+      </Grid>
+      
     </>
   );
 }
--- pixel2pattern/src/components/SideBar.jsx
+++ pixel2pattern/src/components/SideBar.jsx
@@ -1,7 +1,10 @@
-
+import { Card, CardContent, Typography, Link } from "@mui/material";
 export default function SideBar() {
     
     return(
-        <></>
+        <Card sx={{ padding: 2}}>
+            <Typography variant="h6" textAlign='center'>Tutorials & Links</Typography>
+            <CardContent></CardContent>
+        </Card>
     )
 }
